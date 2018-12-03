3cb5bb10f20223b6c138c35525f2a1ee2049c15c
Knolchie <haasmatze@gmail.com>
2018-12-03 13:54:37 +0100

    aufgabe 2

---
+++ src/com/company/Main.java
@@ -0,0 +1,30 @@
+package com.company;
+
+import javax.swing.*;
+
+public class Main {
+
+
+    public static void main(String[] args) {
+        partialString( (JOptionPane.showInputDialog("wort eingeben")));
+
+
+    }
+
+    public static void partialString(String a)
+    {
+        if (a.length() <3)
+        {
+            System.out.println("error");
+
+
+        }
+        else
+            {
+            System.out.println(a.substring(a.length()/2));
+            //a.substring ermöglicht Teile eines Strings auszulesen
+                // /2 gibt die 2te hälfte des strings aus
+        }
+
+}
+}
